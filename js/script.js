39cf31f3485a8945a691fcde9c89147b139e62d9
sopromadzem95-crypto <sopromadzem95@gmail.com>
2026-03-21 16:47:40 +0400

    add burger bar functional

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 // click allert
 
 let btnElement = document.getElementById("send-btn");
-btnElement.addEventListener("click", function name(params) {
+btnElement.addEventListener("click", function name(params){
     alert("Thanks For Your Feedback!")  
 })
+
+// burger-bar funcional
+let navBar = document.getElementById("navigation");
+let burgerBar = document.getElementById("burger-bar");
+
+burgerBar.addEventListener('click', function(){
+    navBar.classList.toggle("active");
+})
+
+
